f5698d7137804c6b4abdf9dc81d68e7e0b703979
suojaapuhelin <joona.ranua1@gmail.com>
2026-06-03 11:56:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,34 @@
 <?php
-
 header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
 
 $apiUrl = rtrim(getenv('MCF_API_URL'), '/');
 $user = getenv('MCF_API_USER');
 $key = getenv('MCF_API_KEY');
 
-$ch = curl_init($apiUrl);
+$endpoint = $apiUrl . '/categories?page_size=5';
 
+$ch = curl_init($endpoint);
 curl_setopt_array($ch, [
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_USERPWD => $user . ':' . $key,
     CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
-    CURLOPT_HEADER => true,
+    CURLOPT_HTTPHEADER => [
+        'Accept: application/json'
+    ],
+    CURLOPT_TIMEOUT => 20,
 ]);
 
 $response = curl_exec($ch);
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
+$error = curl_error($ch);
 curl_close($ch);
 
 echo json_encode([
+    'endpoint' => $endpoint,
     'http_code' => $httpCode,
-    'response' => substr($response, 0, 3000)
-], JSON_PRETTY_PRINT);
+    'curl_error' => $error ?: null,
+    'raw_response' => $response,
+    'decoded_response' => $response ? json_decode($response, true) : null
+], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+?>
