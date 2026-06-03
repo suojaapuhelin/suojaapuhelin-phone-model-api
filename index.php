cd155ab1cc2c433f7ad5da66205079d0ea321f2a
suojaapuhelin <joona.ranua1@gmail.com>
2026-06-03 11:39:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 
 echo json_encode([
-  "status" => "ok",
-  "message" => "Suojaapuhelin phone model API toimii"
-], JSON_UNESCAPED_UNICODE);
+    "status" => "ok",
+    "url" => getenv('MCF_API_URL'),
+    "user" => getenv('MCF_API_USER'),
+    "api_key_exists" => !empty(getenv('MCF_API_KEY'))
+], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+?>
